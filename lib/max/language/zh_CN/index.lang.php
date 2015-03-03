fb9fb4555075a5a4bad2ae439051261738aa88e7
wibiaristama <wibi.aristama@gmail.com>
2015-03-03 11:40:43 +0700

    first commit

---
+++ lib/max/language/zh_CN/index.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
++---------------------------------------------------------------------------+
+| Revive Adserver                                                           |
+| http://www.revive-adserver.com                                            |
+|                                                                           |
+| Copyright: See the COPYRIGHT.txt file.                                    |
+| License: GPLv2 or later, see the LICENSE.txt file.                        |
++---------------------------------------------------------------------------+
+*/
+
+// Meta information
+$translation_readable   = "简体中文";
+$translation_maintainer	= "Sah Lee";
+$translation_contact	= "contact@leesah.name";
+
+?>
